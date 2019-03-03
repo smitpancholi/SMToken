e1fc28e6b4440db45353655adf1e984ad59432b0
Smit Pancholi <38408496+smitpancholi@users.noreply.github.com>
2019-03-03 23:51:59 +0530

    Buying tokens successfull

--- contracts/SMTokensale.sol
+++ contracts/SMTokensale.sol
@@ -5,7 +5,10 @@ contract SMTokensale{
 	address admin;//We have not declared admin as public because we dont want this address to be exposed.
 	SMToken public tokenContract;//SMToken is a datatype of the type of our SMToken.sol contract
 	//so tokenContract is of the type SMToken
-	uint256 public tokenPrice; // As we have written public we will get the function names tokenPrice() for free. we dont need to write it.
+	uint256 public tokenPrice; // As we have written public we will get the function names tokenPrice() for free. we dont need to write it.which will return the value to tokenPrice whenever we ask for it in the test
+	uint256 public tokensSold;
+
+	event Sell(address _buyer , uint256 _amount);
 
 	constructor(SMToken _tokenContract , uint256 _tokenPrice) public {
 		//Assign an admin , which will have special priviledges , for eg admin can end the token sale.
@@ -13,9 +16,21 @@ contract SMTokensale{
 		//Token Contract - because we want to actually purchase tokens
 		tokenContract = _tokenContract;
 		tokenPrice = _tokenPrice;
-
-
-		//Token Price
-
+	}
+	function multiply(uint x, uint y) internal pure returns (uint z) {//internal means it can ony be called internally not externally and pure means no transactions will be updated
+        require(y == 0 || (z = x * y) / y == x);
+    }
+	function buyTokens(uint256 _numberOfTokens) public payable{//payable means we can use ether to buy tokens
+		//Require that the value is equal to tokens
+		require (msg.value == multiply(_numberOfTokens,tokenPrice));
+		//Require that the contract has enough tokens
+		require (tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+		//Require that the transfer is successfull.
+		require (tokenContract.transfer(msg.sender,_numberOfTokens));
+		
+		//Keep track of the tokens sold
+		tokensSold += _numberOfTokens;
+		//trigger sell event 
+		emit Sell(msg.sender , _numberOfTokens);
 	}
 }
--- test/SMTokensale.js
+++ test/SMTokensale.js
@@ -1,8 +1,14 @@
+var SMToken = artifacts.require("./SMToken.sol");
 var SMTokensale = artifacts.require("./SMTokensale.sol");
 
 contract('SMTokensale',function(accounts){
+	var tokenInstance;
 	var tokenSaleInstance;
+	var admin  = accounts[0];
 	var tokenPrice = 1000000000000000;//in wei
+	var buyer = accounts[1];
+	var tokensAvailable = 750000;
+	var numberOfTokens;
 
 	it('initializes the contract with the correct values',function(){
 		return SMTokensale.deployed().then(function(instance){
@@ -16,6 +22,41 @@ contract('SMTokensale',function(accounts){
 			return tokenSaleInstance.tokenPrice();
 		}).then(function(price){
 			assert.equal(price,tokenPrice,'token price is correct');
+		});
+	});
+
+	it('facilitates token buying',function(){
+		return SMToken.deployed().then(function(instance){
+			tokenInstance = instance;
+			return SMTokensale.deployed();
+		}).then(function(instance){
+			tokenSaleInstance = instance;
+			//Provision 75% of all tokens to the token sale
+			return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable , { from : admin} );
+		}).then(function(receipt){ 
+			numberOfTokens = 10;
+			return tokenSaleInstance.buyTokens(numberOfTokens,{ from : buyer , value :numberOfTokens * tokenPrice});
+		}).then(function(receipt){
+			assert.equal(receipt.logs.length, 1, 'triggers one event');
+		    assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+		    assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+		    assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+			return tokenSaleInstance.tokensSold();
+		}).then(function(amount){
+			assert.equal(amount.toNumber(),numberOfTokens,'increments the number of tokens sold');
+			return tokenInstance.balanceOf(buyer);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(),numberOfTokens);
+			return tokenInstance.balanceOf(tokenSaleInstance.address);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(),tokensAvailable - numberOfTokens);
+			//Try to buy tokens different from the ether value;
+		    return tokenSaleInstance.buyTokens(numberOfTokens , { from : buyer , value :1});
+		}).then(assert.fail).catch(function(error){
+			assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+			return tokenSaleInstance.buyTokens(800000 , { from : buyer , value : numberOfTokens * tokenPrice });
+		}).then(assert.fail).catch(function(error){
+			assert(error.message.indexOf('revert') >= 0, 'cannot purchase token more than existing amount');
 		});
 	});
 })
